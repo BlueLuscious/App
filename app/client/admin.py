b979f04d3d628a9c8cc466f3a526cb498a95150a
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-09-13 22:44:22 -0300

    update client
    create new model (clientmodel)
    add clientmodel to admin
    new migration

--- app/client/admin.py
+++ app/client/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from client.models import ClientModel
 
-# Register your models here.
+@admin.register(ClientModel)
+class ClientAdmin(admin.ModelAdmin):
+    pass
--- app/client/models.py
+++ app/client/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class BusinessLineChoices(models.TextChoices):
+    JEWELER = "Jeweler"
+    BEATMAKER = "Beatmaker"
+    OTHERS = "Others"
+
+class ClientModel(AbstractUser):
+    cuit = models.CharField(max_length=11)
+    work_line = models.CharField(choices=BusinessLineChoices.choices, max_length=128, default=BusinessLineChoices.OTHERS.value)
+    interest_line = models.CharField(choices=BusinessLineChoices.choices, max_length=128, default=BusinessLineChoices.OTHERS.value)
+    is_seller = models.BooleanField(default=False)
+    is_buyer = models.BooleanField(default=True)
+    state = models.CharField(max_length=128)
+    city  = models.CharField(max_length=128)
+    postal_code = models.CharField(max_length=4)
+    phone_number = models.CharField(max_length=64)
