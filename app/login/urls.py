cd1862416d405ad41e5a158ecd1e13e49200ca1f
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-09-17 18:24:11 -0300

    update urls.pt (login)
    name url fixed

--- app/login/urls.py
+++ app/login/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from login.views import LogInView
 
 urlpatterns = [
-    path('', LogInView.as_view(), name='signup'),
+    path('', LogInView.as_view(), name='login'),
 ]
