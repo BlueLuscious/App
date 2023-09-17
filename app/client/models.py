690fc02e13c783d60e04dcae234a83b6bde89ccb
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-09-17 18:44:36 -0300

    update models.py (client)
    upload_to avatar folder fixed
    rename TextChoices

--- app/client/models.py
+++ app/client/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class BusinessLineChoices(models.TextChoices):
+class WorkLineChoices(models.TextChoices):
     JEWELER = "Jeweler"
     BEATMAKER = "Beat maker"
     OTHERS = "Others"
 
 class ClientModel(AbstractUser):
     cuit = models.CharField(max_length=11)
-    work_line = models.CharField(choices=BusinessLineChoices.choices, max_length=128, default=BusinessLineChoices.OTHERS.value)
-    interest_line = models.CharField(choices=BusinessLineChoices.choices, max_length=128, default=BusinessLineChoices.OTHERS.value)
+    work_line = models.CharField(choices=WorkLineChoices.choices, max_length=128, default=WorkLineChoices.OTHERS.value)
+    interest_line = models.CharField(choices=WorkLineChoices.choices, max_length=128, default=WorkLineChoices.OTHERS.value)
     is_seller = models.BooleanField(default=False)
     is_buyer = models.BooleanField(default=True)
     state = models.CharField(max_length=128)
     city  = models.CharField(max_length=128)
     postal_code = models.CharField(max_length=4)
     phone_number = models.CharField(max_length=64)
-    avatar = models.ImageField(upload_to="avatar", blank=True)
+    avatar = models.ImageField(upload_to="avatar/", blank=True)
     
     def __str__(self) -> str:
         return f"First name: {self.first_name}, Last name: {self.last_name}"
