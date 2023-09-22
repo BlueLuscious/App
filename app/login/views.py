5105fb022c8202c804108535a6e37cc10d8f35a8
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-09-21 21:14:58 -0300

    update views.py (login)
    add loginrequiredmixin

--- app/login/views.py
+++ app/login/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.views import View
 from django.template import loader
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class LogInView(View):
+class LogInView(LoginRequiredMixin, View):
     def get(self, request):
-        template = loader.get_template('login.html')
+        template = loader.get_template('home.html')
         return HttpResponse(template.render(None, request))
