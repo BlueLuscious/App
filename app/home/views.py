c3a093bfcd6ad718d5c18dfc4aa39d7375b246a0
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-09-22 03:07:03 -0300

    update views.py (home)
    add a view to home
    with LoginRequiredMixin

--- app/home/views.py
+++ app/home/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.template import loader
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class HomeView(LoginRequiredMixin, View):
+    def get(self, request):
+        template = loader.get_template('home.html')
+        return HttpResponse(template.render(None, request))
