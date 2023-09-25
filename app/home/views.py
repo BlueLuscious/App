f8bfd7d0f48a2c61c65679ad207d554e8104ab82
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-09-25 07:04:20 -0300

    update views.py (home)
    add content from stockmodel

--- app/home/views.py
+++ app/home/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.views import View
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
+from stock.models import StockModel
 
 class HomeView(LoginRequiredMixin, View):
     def get(self, request):
         template = loader.get_template('home.html')
-        return HttpResponse(template.render(None, request))
+        context = {
+            "stock": StockModel.objects.filter(type_work_line=request.user.interest_line).exclude(seller=request.user)
+        }
+        return HttpResponse(template.render(context, request))
+    
+    def post(self, request):
+        pass
+    
