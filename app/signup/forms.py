1e7f3df6a51e8b69dd6e8345cc0f66431f32b4fc
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-09-27 05:59:51 -0300

    update forms.py (signup)
    add placeholder to some
    put the fields in order

--- app/signup/forms.py
+++ app/signup/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from client.models import ClientModel
 
 class SignUpForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    repeat_password = forms.CharField(widget=forms.PasswordInput)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    cuit = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Cuit'}))
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Phone Number'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    repeat_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat Password'}))
 
     class Meta:
         model = ClientModel
-        fields = ['email', 'username', 'first_name', 'last_name', 'cuit', 'password', 'repeat_password', 'work_line', 'interest_line', 'is_seller', 'is_buyer', 'state', 'city', 'postal_code', 'phone_number']
+        fields = ['first_name', 'last_name', 'cuit', 'phone_number', 'email', 'username', 'password', 'repeat_password', 'is_seller', 'work_line', 'is_buyer', 'interest_line', 'state', 'city', 'postal_code']
         
