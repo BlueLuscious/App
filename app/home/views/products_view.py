c5e19715b331c4aec88d698e926d509532fd0da3
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-02 18:53:21 -0300

    create product_view.py (home)
    create a view to the products

---
+++ app/home/views/products_view.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.views import View
+from django.template import loader
+from django.contrib.auth.mixins import LoginRequiredMixin
+from stock.models import StockModel
+
+class ProductsView(LoginRequiredMixin, View):
+    def get(self, request):
+        template = loader.get_template('products.html')
+        stock_by_interest_line = StockModel.objects.filter(type_work_line=request.user.interest_line).exclude(seller=request.user)
+        context = {
+            "stock": stock_by_interest_line,
+        }
+        return HttpResponse(template.render(context, request))
+    
+    def post(self, request):
+        pass
