10e3bdf171777010a83317e2170185dede750ef0
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-02 18:50:31 -0300

    update home_view.py
    move and rename views.py (home) to home_view.py

--- app/home/views/home_views.py
+++ app/home/views/home_views.py
@@ -7,8 +7,9 @@ from stock.models import StockModel
 class HomeView(LoginRequiredMixin, View):
     def get(self, request):
         template = loader.get_template('home.html')
+        stock_by_interest_line = StockModel.objects.filter(type_work_line=request.user.interest_line).exclude(seller=request.user)
         context = {
-            "stock": StockModel.objects.filter(type_work_line=request.user.interest_line).exclude(seller=request.user)
+            "stock": stock_by_interest_line,
         }
         return HttpResponse(template.render(context, request))
     
