d29d52ca24830095468d1849bae498732d021fd0
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-04 20:58:30 -0300

    update products_view.py (home)
    change stock by interest, get all stock
    add logged in user data (ClientModel)

--- app/home/views/products_view.py
+++ app/home/views/products_view.py
@@ -3,13 +3,16 @@ from django.views import View
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
 from stock.models import StockModel
+from client.models import ClientModel
 
 class ProductsView(LoginRequiredMixin, View):
     def get(self, request):
         template = loader.get_template('products.html')
-        stock_by_interest_line = StockModel.objects.filter(type_work_line=request.user.interest_line).exclude(seller=request.user)
+        stock_by_interest_line = StockModel.objects.all().exclude(seller=request.user.id)
+        userdata = ClientModel.objects.get(id=request.user.id)        
         context = {
             "stock": stock_by_interest_line,
+            "userdata": userdata,
         }
         return HttpResponse(template.render(context, request))
     
