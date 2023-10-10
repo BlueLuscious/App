4cbc0d6360f077cf1ff3aa48acd531bee99fd788
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-10 07:14:06 -0300

    update urls.py (home)
    - add path to CartView

--- app/home/urls.py
+++ app/home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from home.views.home_view import HomeView
 from home.views.products_view import ProductsView
+from home.views.cart_view import CartView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/', ProductsView.as_view(), name='products'),
+    path('cart/', CartView.as_view(), name='cart'),
 ]
