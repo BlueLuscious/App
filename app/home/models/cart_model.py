84926ae14c0d551f02499d756c5664892d6583bc
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-17 04:38:58 -0300

    update cart_model.py (home/models)
    remove default to quantity

--- app/home/models/cart_model.py
+++ app/home/models/cart_model.py
@@ -5,7 +5,7 @@ from client.models import ClientModel
 class CartModel(models.Model):
     buyer = models.ForeignKey(ClientModel, on_delete=models.CASCADE)
     product = models.ForeignKey(StockModel, on_delete=models.CASCADE)
-    quantity = models.FloatField(max_length=128, default=1)
+    quantity = models.FloatField(max_length=128)
     
     class Meta:
         unique_together = ('buyer', 'product')
