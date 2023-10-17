778607a4c622fc43f303a12b7dcef19dbf9e4777
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-17 04:38:05 -0300

    update admin.py (home)
    CartAdmin

--- app/home/admin.py
+++ app/home/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from home.models.cart_model import CartModel
 
-# Register your models here.
 @admin.register(CartModel)
-class CartModel(admin.ModelAdmin):
+class CartAdmin(admin.ModelAdmin):
     pass
