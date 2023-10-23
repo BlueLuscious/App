b7a17bc8d18ab9828ccf56303c37c25b7e138c8b
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-23 04:43:14 -0300

    update home/services
    cart_service.py:
    - created CartServices, added get_context method
    - fix total price (comment)

--- app/home/services/cart_service.py
+++ app/home/services/cart_service.py
@@ -1,10 +1,14 @@
-from stock.models import StockModel
+#from django.db.models import Sum, F
+from home.models.cart_model import CartModel
 from client.models import ClientModel
 
 class CartServices:
     @staticmethod
-    def get_context(id):
+    def get_context(buyer, id):
+        cart_buyer = CartModel.objects.filter(buyer=buyer)
         userdata = ClientModel.objects.get(id=id)
         return {
             "userdata": userdata,
+            "cart_data": cart_buyer,
+            #"total_price": cart_buyer.aggregate(sum=Sum(F('product.price')*F('quantity')))["sum"]
         }
