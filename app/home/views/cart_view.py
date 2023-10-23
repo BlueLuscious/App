5b062670f0a12fe40925c7ec9863d5cae6533113
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-23 04:41:02 -0300

    update home/views
    cart_view.py:
    - added post method. In it, added a handler to check if the cart was created and add products to the previously added ones or if it wasn't, it's created
    - get context from cart_service.py

--- app/home/views/cart_view.py
+++ app/home/views/cart_view.py
@@ -2,16 +2,32 @@ from django.http import HttpResponse
 from django.views import View
 from django.template import loader
 from django.contrib.auth.mixins import LoginRequiredMixin
+from stock.models import StockModel
+from home.models.cart_model import CartModel
 from home.services.cart_service import CartServices
 
 template = loader.get_template('cart.html')
 
 class CartView(LoginRequiredMixin, View):
     def get(self, request):
-        context = CartServices.get_context(request.user.id)
+        context = CartServices.get_context(request.user, request.user.id)
         return HttpResponse(template.render(context, request))
     
     def post(self, request):
-        context = CartServices.get_context(request.user.id)
+        stock = StockModel.objects.get(id=request.POST.get("id"))
+        cart_items = CartModel.objects.filter(buyer=request.user, product=stock).first()
+        
+        if cart_items:
+            cart_items.quantity += float(request.POST.get("quantity"))
+            cart_items.save()
+        else:
+            data = {
+                "buyer": request.user,
+                "product": stock,
+                "quantity": float(request.POST.get("quantity"))
+            }
+            CartModel.objects.create(**data)
+        
+        context = CartServices.get_context(request.user, request.user.id)
         return HttpResponse(template.render(context, request))
     
